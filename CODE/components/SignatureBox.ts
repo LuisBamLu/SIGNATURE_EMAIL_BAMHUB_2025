11be78b7b4ba4c32311df6e5c39130ba8a5d980a
LuisBamLu <luis@groupbam.com>
2025-07-18 15:48:21 -0300

    added centralized box imports

---
+++ CODE/components/SignatureBox.ts
@@ -0,0 +1,12 @@
+import NameBox from "./NameBox.ts";
+import HomeBox from "./HomeBox.ts";
+import MediaBox from "./MediaBox.ts";
+import EmbassyBox from "./EmbassyBox.ts";
+
+export
+{
+    NameBox,
+    HomeBox,
+    MediaBox,
+    EmbassyBox
+}
